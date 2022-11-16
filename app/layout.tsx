776adb47cd0bee8b1592e07a7da6c8b491750568
kami988 <baske.yoshi007@gmail.com>
2022-11-17 02:08:02 +0900

    add: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,49 @@
-import './globals.css'
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
 
-export default function RootLayout({
-  children,
-}: {
+type LayoutProps = Required<{
   children: React.ReactNode
-}) {
+}>
+
+export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='ja'>
       <head />
-      <body>{children}</body>
+      <body>
+        <div className='flex min-h-screen flex-col items-center justify-center text-sm text-gray-600'>
+          <header>
+            <nav className='w-screen bg-gray-800'>
+              <div className='flex h-14 items-center pl-8'>
+                <div className='flex space-x-2'>
+                  <Link href='/'>
+                    <span className='rounded px-3 py-2 text-gray-300 hover:bg-gray-700'>Home</span>
+                  </Link>
+                  <Link href='/pokemon'>
+                    <span className='rounded px-3 py-2 text-gray-300 hover:bg-gray-700'>
+                      Pokemon
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </nav>
+          </header>
+          <main className='flex w-screen flex-1 flex-col items-center justify-center'>
+            {children}
+          </main>
+          <footer className='flex h-12 w-full items-center justify-center border-t-2'>
+            <a
+              className='flex items-center'
+              href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Powered by {/* <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" /> */}
+              <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+            </a>
+          </footer>
+        </div>
+      </body>
     </html>
   )
 }
